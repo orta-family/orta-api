8e498a7d26191181d2f7b21bd4c846caec7c94f8
Sophie Orta <sophie@travis-ci.org>
2020-01-29 20:39:02 -0500

    Generate PG_URL if needed

--- app/config.ts
+++ app/config.ts
@@ -7,9 +7,16 @@ export const {
   OA_PG_HOST: PG_HOST,
   OA_PG_PORT: PG_PORT,
   OA_DB_NAME: DB_NAME,
-  OA_PG_URL: PG_URL,
   OA_PORT: PORT = 3333,
 } = process.env;
+
+let { OA_PG_URL } = process.env;
+
+if (!OA_PG_URL) {
+  OA_PG_URL = `postgres://${PG_USER}:${PG_PASS}@${PG_HOST}:${PG_PORT}/${DB_NAME}`;
+}
+
+export const PG_URL = OA_PG_URL;
 
 export default {
   ENV,
