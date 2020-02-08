268cccdf55df6da53d941f8410b2cdf1b0f86801
Sophie Orta <sophie@travis-ci.org>
2020-02-07 20:08:36 -0500

    Rearrange a bit

--- app/server.ts
+++ app/server.ts
@@ -1,19 +1,29 @@
 import { PORT } from './config';
 import 'reflect-metadata';
+import chalk from 'chalk';
 import express from 'express';
 
-const app = express();
+async function main() {
+  console.log('Starting Orta API...');
+  const app = express();  
 
-const main = async () => {
   app.use(express.json());
 
   app.get('/', (req, res) => res.json({ message: 'Test' }));
 
+  app.get('/members', (req, res) => res.json({ message: 'Test' }));
+
   app.use((req, res) => res.status(404).json({ message: 'This is not a valid endpoint' }));
 
   app.listen(PORT, () => {
-    console.log(`Orta API listening on http://localhost:${PORT}`);
+    console.log(chalk.green(
+      `${chalk.bold('Orta API')} listening on ${chalk.underline(`http://localhost:${PORT}`)}`
+    ));
   });
 };
 
-main();
+main().catch(e => {
+  console.error("Fatal error occurred starting server!");
+  console.error(e);
+  process.exit(101);
+});
