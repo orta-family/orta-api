a7ce2697716503a8383122c6c93e403b0acac3fa
Sophie Orta <sophie@travis-ci.org>
2020-02-11 20:01:23 -0500

    Add some basic validation and findBySlug

--- app/entity/Slug.ts
+++ app/entity/Slug.ts
@@ -1,21 +1,25 @@
 import { BeforeInsert, BeforeUpdate, Column } from 'typeorm';
+import { Length } from "class-validator";
 import slugify from 'slugify';
 import { Base } from './Base';
 
 export abstract class Slug extends Base {
     @Column()
+    @Length(1)
     name!: string;
 
     @Column({ unique: true })
     slug!: string;
 
     @BeforeInsert()
-    public setSlugBeforeInsert(): void {
+    @BeforeUpdate()
+    public setSlugFromName(): void {
       this.slug = slugify(this.name);
     }
 
-    @BeforeUpdate()
-    public setSlugBeforeUpdate(): void {
-      this.slug = slugify(this.name);
+    static findBySlug(slug: string) {
+      return this.createQueryBuilder('s')
+        .where('s.slug = :slug', { slug })
+        .getOne();
     }
 }
