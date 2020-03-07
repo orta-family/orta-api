edfc33494c06eb13ca81b6690ccd85ef3affdf5b
Sophie Orta <sophie@travis-ci.org>
2020-03-07 13:49:38 -0500

    Remove comments

--- app/routes/rest/index.ts
+++ app/routes/rest/index.ts
@@ -1,11 +1,7 @@
 import express from 'express';
 import membersRouter from './members';
-// import { CrudRouter } from '~/routes/Crud';
-// import { Member } from '~/entity/Member';
 
 const router = express.Router();
-
-// const crud = new CrudRouter(Member);
 
 router.use('/members', membersRouter);
 
