2ce4981df48167eb8b568e46dfd1c4e52522e808
Sophie Orta <sophie@travis-ci.org>
2020-09-03 11:02:01 -0400

    Run nest new

--- app/routes/rest/index.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import membersRouter from './members';
-
-const router = express.Router();
-
-router.use('/members', membersRouter);
-
-export default router;
--- app/routes/rest/members.ts
+++
@@ -1,7 +0,0 @@
-import { Member } from '~/entity/Member';
-import { SlugCrudRouter, CrudRouter } from '~/routes/Crud';
-
-const memberCrud = new SlugCrudRouter(Member, { name: 'Member' });
-
-export default memberCrud.router;
-export { memberCrud };
